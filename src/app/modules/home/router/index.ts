64f1c5bdfc63e92674d77f6f4a521461f36d32dc
pickman murimi <pickmanmurimi@gmail.com>
2021-11-29 00:29:16 +0300

    Use alias on routes

--- src/app/modules/home/router/index.ts
+++ src/app/modules/home/router/index.ts
@@ -1,16 +1,7 @@
 export default [
     {
         path: '/',
-        component: () => import('../views/Friends.vue'),
-        name: 'Home',
-        redirect: { name: 'Friends'},
-        meta: {
-            requiresAuth: false,
-            title: 'Home',
-        },
-    },
-    {
-        path: '/@me/friends',
+        alias: '/@me/friends',
         component: () => import('../views/Friends.vue'),
         name: 'Friends',
         meta: {
