1f05a6fb2b6b42c143b0f88749f6446159360128
pickman murimi <pickmanmurimi@gmail.com>
2021-11-29 00:57:37 +0300

    Load direct messages from json file

--- src/app/modules/home/components/HomeSideMenu.vue
+++ src/app/modules/home/components/HomeSideMenu.vue
@@ -12,16 +12,16 @@
       <ul class="mt-2 sidemenu-px-10">
         <!--    friends-->
         <li class="sidemenu-items flex items-center">
-          <router-link :to="{ name: 'Friends'}" class="sidemenu-item"
-          :class="{ 'sidemenu-item-active' : route.name === 'Friends'}">
+          <router-link :class="{ 'sidemenu-item-active' : route.name === 'Friends'}" :to="{ name: 'Friends'}"
+                       class="sidemenu-item">
             <FriendsIcon class="mr-4"/>
             <span class="font-medium">Friends</span>
           </router-link>
         </li>
         <!--    nitro-->
         <li class="sidemenu-items flex items-center">
-          <router-link :to="{ name: 'Nitro'}" class="sidemenu-item"
-                       :class="{ 'sidemenu-item-active' : route.name === 'Nitro'}">
+          <router-link :class="{ 'sidemenu-item-active' : route.name === 'Nitro'}" :to="{ name: 'Nitro'}"
+                       class="sidemenu-item">
             <NitroIcon class="mr-4"/>
             <span class="font-medium">Nitro</span>
           </router-link>
@@ -37,83 +37,25 @@
       <!--    messages-->
       <ul class="pl-3 pr-3 mt-3">
         <!--      message 1-->
-        <li class="flex mb-0.5 items-center dark:hover:bg-discord-black-200 hover:bg-gray-100
+        <li v-for="dm in DirectMessages" :key="dm.name" class="flex mb-0.5 items-center dark:hover:bg-discord-black-200 hover:bg-gray-100
        p-1 rounded cursor-pointer group relative">
           <div class="mr-3 relative">
-            <img alt="irup io logo" class="object-cover w-full h-full dm-thumbnail rounded-full
-           border border-gray-300 group-hover:border-0"
-                 src="@assets/placeholders/tailwind.png">
-            <span class="absolute status-indicator-wrapper bg-discord-black-400 rounded-full">
-            <span class="status-indicator bg-green-400 rounded-full"></span>
-          </span>
+            <img :src="dm.image" :alt="dm.name"
+                 class="object-cover w-full h-full dm-thumbnail
+                 rounded-full border border-discord-black-300 group-hover:border-0">
+            <span v-if="dm.online" class="absolute status-indicator-wrapper bg-discord-black-400 rounded-full">
+              <span class="status-indicator bg-green-400 rounded-full"></span>
+            </span>
           </div>
           <div>
             <span
-                class="text-sm block text-gray-400 font-medium menu-item-title group-hover:text-gray-500 dark:group-hover:text-gray-200">Tailwind</span>
-            <small class="text-xs block text-gray-400 group-hover:text-gray-500 dark:group-hover:text-gray-100">In the
-              mood for a hug</small>
-          </div>
-          <span class="group-hover:block hidden hover:text-gray-200 absolute right-0 text-gray-400 mr-1">
-          <CloseIcon class="icon-16"/>
-        </span>
-        </li>
-
-        <!--      message 2-->
-        <li class="flex mb-0.5 items-center dark:hover:bg-discord-black-200 hover:bg-gray-100
-       p-1 rounded cursor-pointer group relative">
-          <div class="mr-3 relative">
-            <img alt="irup io logo" class="object-cover w-full h-full dm-thumbnail rounded-full bg-gray-50
-          border border-gray-300 group-hover:border-0"
-                 src="@assets/placeholders/discord-logo.png">
-            <span class="absolute status-indicator-wrapper bg-discord-black-400 rounded-full">
-            <span class="status-indicator bg-green-400 rounded-full"></span>
-          </span>
-          </div>
-          <div>
-            <span
-                class="text-sm text-gray-400 font-medium menu-item-title group-hover:text-gray-500 dark:group-hover:text-gray-200">CedricPotato</span>
-          </div>
-          <span class="group-hover:block hidden hover:text-gray-200 absolute right-0 text-gray-400 mr-1">
-          <CloseIcon class="icon-16"/>
-        </span>
-        </li>
-
-        <!--      message 3-->
-        <li class="flex mb-0.5 items-center dark:hover:bg-discord-black-200 hover:bg-gray-100
-       p-1 rounded cursor-pointer group relative">
-          <div class="mr-3 relative">
-            <img alt="irup io logo" class="object-cover w-full h-full dm-thumbnail rounded-full bg-gray-50
-          border border-gray-300 group-hover:border-0"
-                 src="@assets/placeholders/icons8-discord-50.png">
-            <span class="absolute status-indicator-wrapper bg-discord-black-400 rounded-full">
-            <span class="status-indicator bg-green-400 rounded-full"></span>
-          </span>
-          </div>
-          <div>
-            <span
-                class="text-sm text-gray-400 font-medium menu-item-title group-hover:text-gray-500 dark:group-hover:text-gray-200">iam.tony</span>
-          </div>
-          <span class="group-hover:block hidden hover:text-gray-200 absolute right-0 text-gray-400 mr-1">
-          <CloseIcon class="icon-16"/>
-        </span>
-        </li>
-
-
-        <!--      message 4-->
-        <li class="flex mb-0.5 items-center dark:hover:bg-discord-black-200 hover:bg-gray-100
-       p-1 rounded cursor-pointer group relative">
-          <div class="mr-3 relative">
-            <img alt="irup io logo" class="object-cover w-full h-full dm-thumbnail rounded-full bg-gray-50
-          border border-gray-300 group-hover:border-0"
-                 src="@assets/placeholders/logosmalltransparent.png">
-            <span class="absolute status-indicator-wrapper bg-discord-black-400 rounded-full">
-            <span class="status-indicator bg-green-400 rounded-full"></span>
-          </span>
-          </div>
-          <div>
-            <span
-                class="text-sm text-gray-400 font-medium menu-item-title group-hover:text-gray-500 block dark:group-hover:text-gray-100">Nova.</span>
-            <small class="text-xs block text-gray-400 group-hover:text-gray-500 dark:group-hover:text-gray-100">https://irup.io</small>
+                class="text-sm block text-gray-400 font-medium menu-item-title group-hover:text-gray-500 dark:group-hover:text-gray-200">
+              {{ dm.name }}
+            </span>
+            <small v-if="dm.tagline"
+                   class="text-xs block text-gray-400 group-hover:text-gray-500 dark:group-hover:text-gray-100">
+              {{ dm.tagline }}
+            </small>
           </div>
           <span class="group-hover:block hidden hover:text-gray-200 absolute right-0 text-gray-400 mr-1">
           <CloseIcon class="icon-16"/>
@@ -156,6 +98,7 @@ import NitroIcon from "@/app/common/components/icons/NitroIcon.vue";
 import PlusIcon from "@/app/common/components/icons/PlusIcon.vue";
 import CloseIcon from "@/app/common/components/icons/CloseIcon.vue";
 import {useRoute} from "vue-router";
+import DirectMessages from "@/app/common/data/directMessages.json"
 
 const route = useRoute();
 
