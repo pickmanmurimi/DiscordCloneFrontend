2897475303ab1f8dce85a0b825148429665e97d0
pickman murimi <pickmanmurimi@gmail.com>
2021-12-06 03:42:17 +0300

    Added a mobile placeholder for small screens

---
+++ src/app/common/components/placeholders/MobilePlaceholder.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="dark:bg-discord-black-500 min-h-screen w-full lg:hidden bg-gray-100 pb-20">
+    <div class="flex w-full flex justify-center">
+      <img src="/images/placeholders/discord-logo.png" class="animate-pulse" width="200" height="200">
+    </div>
+
+    <div class="text-left mx-5">
+      <h1 class="text-discord-black-60 text-xl">
+        Thanks for checking out my work! unfortunately 😩, the mobile version is not ready yet.
+        You can only view this clone from a computer screen 💻🖥️.
+      </h1>
+      <h1 class="text-discord-black-60 text-xl mt-10">
+        Am working on the mobile version 😀.
+        I will surely update on my socials once ready.
+      </h1>
+    </div>
+
+    <div class="text-discord-black-60 text-xl mx-5 mt-10 border-green-400 border-l-4 pl-4">
+      <span class="mr-2 block mb-2">@linkedin</span>
+      <a href="https://www.linkedin.com/in/pickman-murimi/"
+         class="px-5 py-0.5 border dark:border-gray-700 border-gray-300 hover:shadow rounded"> Pickman Murimi </a>
+      <span class="mr-2 block my-2">Some of my work</span>
+      <a href="https://www.linkedin.com/in/pickman-murimi/"
+         class="px-5 py-0.5 border dark:border-gray-700 border-gray-300 hover:shadow rounded"> irup.io </a>
+    </div>
+
+
+  </div>
+</template>
